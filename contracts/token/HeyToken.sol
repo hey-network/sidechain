61715176bbab13c05fd0b96e764d60a343706827
thomasvds <vanderstraeten.thomas@gmail.com>
2018-10-11 18:16:08 +0200

    Make the HEY token a StandardBurnableToken

--- contracts/token/HeyToken.sol
+++ contracts/token/HeyToken.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.24;
 
-import "./ERC20/StandardToken.sol";
+import "./ERC20/StandardBurnableToken.sol";
 import "./ERC20/MintableToken.sol";
 
 /**
  * @title HeyToken
- * @dev An out-of-the-box mintable ERC20 token
+ * @dev An out-of-the-box mintable and burnable ERC20 token
  */
-contract HeyToken is StandardToken, MintableToken {
+contract HeyToken is StandardBurnableToken, MintableToken {
   string public name = "HeyToken";
   string public symbol = "HEY";
   uint8 public decimals = 18;
